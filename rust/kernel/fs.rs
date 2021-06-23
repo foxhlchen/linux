1a4eeb474b7ebd6bf821af049abbb0ffe48dae7a
Fox Chen <foxhlchen@gmail.com>
2021-06-23 22:53:28 +0800

    update
    Signed-off-by: Fox Chen <foxhlchen@gmail.com>

--- rust/kernel/fs.rs
+++ rust/kernel/fs.rs
@@ -4,9 +4,10 @@
 
 use crate::bindings::{
     dentry, file_system_type, inode, mount_bdev, mount_nodev, mount_single, super_block, register_filesystem,
+    unregister_filesystem
 };
 use crate::str::*;
-use crate::{c_types, error::Error, Result, ThisModule};
+use crate::{c_types, error::Error, Result, ThisModule, c_str};
 use core::ptr;
 use alloc::boxed::Box;
 
@@ -17,8 +18,16 @@ unsafe extern "C" fn mount_callback<T: FileSystem>(
     data: *mut c_types::c_void,
 ) -> *mut dentry {
     let r_fs_type = FSType::from_c_fs_type(fs_type).unwrap();
-    let r_dev_name = unsafe { CStr::from_char_ptr(dev_name) };
-    let r_data = unsafe { CStr::from_char_ptr(data as *const c_types::c_char) };
+    let r_dev_name = if dev_name.is_null() {
+        c_str!("")
+    } else {
+        unsafe { CStr::from_char_ptr(dev_name) }
+    };
+    let r_data = if data.is_null() { 
+        c_str!("")
+    } else { 
+        unsafe { CStr::from_char_ptr(data as *const c_types::c_char) }
+    };
 
     let rt: Result<Dentry> = match T::MOUNT_TYPE {
         MountType::Custom => T::mount(&r_fs_type, flags, r_dev_name, r_data),
@@ -58,7 +67,11 @@ unsafe extern "C" fn fill_super_callback<T: FileSystem>(
     }
 
     let r_sb = r_sb_rs.unwrap();
-    let r_data = unsafe { CStr::from_char_ptr(data as *const c_types::c_char) };
+    let r_data = if data.is_null() { 
+        c_str!("")
+    } else { 
+        unsafe { CStr::from_char_ptr(data as *const c_types::c_char) }
+    };
 
     let rs = T::fill_super(&r_sb, r_data, silent as i32);
     if let Err(e) = rs {
@@ -192,14 +205,18 @@ pub enum MountType {
     Single,
 }
 
-pub trait FileSystem {
+pub type FSHandle = Box::<file_system_type>;
+
+pub trait FileSystem: Sized + Sync {
     const MOUNT_TYPE: MountType;
 
     fn mount(fs_type: &FSType, flags: i32, dev_name: &CStr, data: &CStr) -> Result<Dentry> {
+        crate::pr_warn!("mount fs");
         Err(Error::EINVAL)
     }
 
     fn fill_super(sb: &SuperBlock, data: &CStr, silent: i32) -> Result<()> {
+        crate::pr_warn!("fill super");
         Err(Error::EINVAL)
     }
 
@@ -207,7 +224,7 @@ pub trait FileSystem {
 
     }
 
-    fn register_self(name: &'static CStr, owner: ThisModule) -> Result<()> where Self: Sized {
+    fn register_self(name: &'static CStr, owner: &ThisModule) -> Result<FSHandle> where Self: Sized {
         let mut c_fs_type = Box::new(file_system_type::default());
         c_fs_type.mount = Some(mount_callback::<Self>);
         c_fs_type.kill_sb = Some(kill_sb_callback::<Self>);
@@ -215,6 +232,15 @@ pub trait FileSystem {
         c_fs_type.name = name.as_char_ptr();
 
         let err = unsafe { register_filesystem(c_fs_type.as_mut() as *mut _) };
+        if err != 0 {
+            return Err(Error::from_kernel_errno(err));
+        }
+
+        Ok(c_fs_type)
+    }
+
+    fn unregister_self(c_fs_type: &mut FSHandle) -> Result<()> {
+        let err = unsafe{ unregister_filesystem(c_fs_type.as_mut() as *mut _) };
         if err != 0 {
             return Err(Error::from_kernel_errno(err));
         }
--- samples/rust/rust_ramfs.rs
+++ samples/rust/rust_ramfs.rs
@@ -6,8 +6,11 @@
 #![feature(allocator_api, global_asm)]
 
 use kernel::prelude::*;
+use kernel::fs::*;
+use kernel::c_str;
 
-struct Ramfs;
+struct Ramfs {
+}
 
 module! {
     type: Ramfs,
@@ -17,12 +20,26 @@ module! {
     license: b"GPL v2",
 }
 
+static mut FS_HANDLE: Option<FSHandle> = None;
+
+impl FileSystem for Ramfs {
+    const MOUNT_TYPE: MountType = MountType::Custom;
+}
+
 impl KernelModule for Ramfs {
     fn init() -> Result<Self> {
+        unsafe { FS_HANDLE = Some(Ramfs::register_self(c_str!("rust_ramfs"), &THIS_MODULE)?) };
+        pr_warn!("register rust fs");
         Ok(Self {})
     }
 }
 
 impl Drop for Ramfs {
-    fn drop(&mut self) {}
+    fn drop(&mut self) {
+        unsafe {
+            if let Some(fshd) = &mut FS_HANDLE {
+                Ramfs::unregister_self(fshd);
+            }
+        }
+    }
 }
